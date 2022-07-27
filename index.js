584347a2002d21137ee503e9cf0f086605cdda54
Rafael <rarcastro@gmail.com>
2022-07-27 16:56:04 -0400

    add html css and js

---
+++ index.js
@@ -0,0 +1,27 @@
+//document.getElementById('count-el').innerText = 5
+
+
+
+let count = 5
+
+
+count = count + 2
+
+console.log(count)
+
+
+let bonusPoints = 50
+
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+
+console.log(bonusPoints)
